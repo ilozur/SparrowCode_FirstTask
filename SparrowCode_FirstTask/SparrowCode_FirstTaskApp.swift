cc63cb9bcc73f56651330c31110d80004a623af2
Edmond <edpodlegaev@yandex.ru>
2023-12-01 12:16:49 +0300

    Initial Commit

---
+++ SparrowCode_FirstTask/SparrowCode_FirstTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparrowCode_FirstTaskApp.swift
+//  SparrowCode_FirstTask
+//
+//  Created by Edmond Podlegaev on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparrowCode_FirstTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
